476469fcde5b940e4769ef2ad228a30412f953ae
ttpapag <teresa.papageorgiou@camelotls.com>
2019-10-27 10:49:13 +0200

    download stats btn - wip

--- src/app/stats/stats.page.ts
+++ src/app/stats/stats.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
+
+interface IDayStat {
+	date: string;
+	mood: string;
+};
 
 @Component({
 	selector: 'app-stats',
@@ -8,24 +14,53 @@ import { Storage } from '@ionic/storage';
 })
 export class StatsPage implements OnInit {
 
-	public days: any[] = [];
-	constructor( private storage: Storage) { }
+	public days: IDayStat[] = [];
+	
+	constructor(
+		private storage: Storage,
+		public alertCtrl: AlertController) { }
 
-	ngOnInit() {
+	ngOnInit(): void {
+		this.loadStoredStats(); 
+	}
+
+	public async onDownload(): Promise<void> {
+		const alert = await this.alertCtrl.create({
+			header: 'Download Progress',
+			message: 'This action will save all your statistics to your device.',
+			buttons: [
+				{
+					text: 'Save',
+					handler: () => this.saveFile()
+				},
+				{
+					text: 'Cancel',
+					role: 'cancel'
+				}
+			]
+		});
+		return await alert.present();
+	}
+
+	private saveFile() {
+		// TODO: save this.days in a file in the device
+	}
+
+	private loadStoredStats(): void {
 		this.storage.keys().then(
-			storedKeys => 
-				storedKeys.forEach( key => {
-					if (this.isDate(key)) 
-						this.storage.get(key).then( happinessValue => 
-							this.days.push({date: key, mood: happinessValue}));
+			storedKeys =>
+				storedKeys.forEach(key => {
+					if (this.isDate(key))
+						this.storage.get(key).then( happinessValue =>
+							this.days.push({ date: key, mood: happinessValue }));
 				})
 		);
 	}
-
+	
 	private isDate(date: string): boolean {
-		const dateModel: RegExp = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i; 
+		const dateModel: RegExp = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
 		return dateModel.test(date);
 	}
 
-	
+
 }
