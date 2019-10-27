02968e18ff2993d1f37d01fd51991aa98728b927
ttpapag <teresa.papageorgiou@camelotls.com>
2019-10-27 09:23:17 +0200

    create stats page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'track-today', loadChildren: './track-today/track-today.module#TrackTodayPageModule' },
+  { path: 'stats', loadChildren: './stats/stats.module#StatsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/stats/stats.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-stats',
+	templateUrl: './stats.page.html',
+	styleUrls: ['./stats.page.scss'],
+})
+export class StatsPage implements OnInit {
+
+	public days;
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
