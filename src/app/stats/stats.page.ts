18ebbe7cca80307323a7b6d94954bfffdb925bd3
ttpapag <teresa.papageorgiou@camelotls.com>
2019-10-27 09:43:41 +0200

    display stats from storage

--- src/app/stats/stats.page.ts
+++ src/app/stats/stats.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
 	selector: 'app-stats',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsPage implements OnInit {
 
-	public days;
-	constructor() { }
+	public days: any[] = [];
+	constructor( private storage: Storage) { }
 
 	ngOnInit() {
+		this.storage.keys().then(
+			res => {
+				console.log(res);
+				res.forEach( date => {
+					if (this.isDate(date)) 
+						this.storage.get(date).then(
+							happinessValue => {
+								console.log( date, happinessValue )
+								this.days.push({date: date, mood: happinessValue});
+							}
+						);
+				})
+			}
+		)
 	}
 
+	private isDate(date: string): boolean {
+		const dateModel: RegExp = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i; 
+		return dateModel.test(date);
+	}
+
+	
 }
